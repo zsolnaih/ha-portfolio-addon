e7bbe58dbf84c7595f559a2839c3fe35bcb7a624
zsolnaih <zsolnaih@gmail.com>
2025-07-16 20:12:41 +0200

    v0.1.0

--- my_portfolio_addon/main.py
+++ my_portfolio_addon/main.py
@@ -46,10 +46,10 @@ def reduce_snapshots(data):
     return sorted(reduced, key=lambda x: x["timestamp"])
 
 # === Config ===
-CREDENTIALS_PATH = "credentials.json"   # Path of credentials
+CREDENTIALS_PATH = "/share/credentials.json"  # Path of credentials
 SHEET_NAME = "Stock Portfolio"            # GoogleSheet
 WORKSHEET_NAME = "Report"                 # Worksheet
-JSON_OUTPUT_PATH = "portfolio.json"     # Output json (in add-on: /share/portfolio.json)
+JSON_OUTPUT_PATH = "/share/portfolio_log.json"    # Output json (in add-on: /share/portfolio.json)
 
 # === Google Sheets API authentication ===
 scope = [
@@ -77,9 +77,19 @@ headers = data[0]
 rows = data[1:]
 
 portfolio_entries = []
+summary = []
 for row in rows:
     if not row[0].strip():
-        continue  # skip the summary row
+        try:
+            summary.append({
+                "buy_price": float(row[4].replace("Ft", "").replace("\xa0", "").replace(",", ".").strip()),
+                "portfolio_value": float(row[5].replace("Ft", "").replace("\xa0", "").replace(",", ".").strip()),
+                "gain_percent": float(row[6].replace("%", "").replace(",", ".").strip()),
+                "gain_huf": float(row[7].replace("Ft", "").replace("\xa0", "").replace(",", ".").replace("- ", "-").strip())
+            })
+        except Exception as e:
+            print(f"Error in row: {row} > {e}")
+        continue
     try:
         portfolio_entries.append({
             "ticker": row[0].strip(),
@@ -96,8 +106,9 @@ for row in rows:
 
 # === New snapshot with TimeStamp ===
 snapshot = {
-    "timestamp": datetime.now().isoformat(),
-    "portfolio": portfolio_entries
+    "timestamp": datetime.now().replace(second=0, microsecond=0).isoformat(),
+    "portfolio": portfolio_entries,
+    "summary": summary
 }
 
 # === Parse the existing portfolio.json ===
@@ -117,6 +128,6 @@ reduced_data = reduce_snapshots(existing_data)
 
 # === Write the new data to portfolio.json ===
 with open(JSON_OUTPUT_PATH, "w") as f:
-    json.dump(existing_data, f, indent=2)
+    json.dump(reduced_data, f, indent=2)
 
 print(f"Data is saved in {JSON_OUTPUT_PATH} ")
